9e16131a3272bc4126c37280dceb31497b2725e7
Do Thanh <doxuanthanh23299@gmail.com>
2023-07-18 16:29:01 +0700

    Edit views

---
+++ resources/views/backend/master/layouts/head.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <base href="/backend/" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body>
