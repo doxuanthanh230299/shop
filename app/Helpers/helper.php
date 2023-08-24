4126c1708c434f2cb8f9b3bca5f04e5a0cf82818
Do Thanh <doxuanthanh23299@gmail.com>
2023-08-24 12:17:18 +0700

    create format currency helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -23,9 +23,9 @@ if (!function_exists('listCategories')) {
             if ($category['parent'] == $parent) {
                 echo '<div class="item-menu"><span>' . $char . $category['name'] . '</span>
                 <div class="category-fix">
-                    <a class="btn-category btn-primary" href="/admin/category/edit/'.$category['id'].'"><i
+                    <a class="btn-category btn-primary" href="/admin/category/edit/' . $category['id'] . '"><i
                             class="fa fa-edit"></i></a>
-                    <a class="btn-category btn-danger" href="/admin/category/delete/'.$category['id'].'"><i
+                    <a class="btn-category btn-danger" href="/admin/category/delete/' . $category['id'] . '"><i
                             class="fas fa-times"></i></i></a>
                 </div>
             </div>';
@@ -33,5 +33,13 @@ if (!function_exists('listCategories')) {
                 listCategories($categories, $new_parent, $char . ' ---| ');
             }
         }
+    }
+}
+
+if (!function_exists('formatCurrency')) {
+    function formatCurrency($amount, $currentSymbol = 'đ', $decimalPlaces = 2, $thousandsSeparator = '.')
+    {
+        $formattedAmount = number_format($amount, $decimalPlaces, ',', $thousandsSeparator);
+        return $formattedAmount . $currentSymbol;
     }
 }
