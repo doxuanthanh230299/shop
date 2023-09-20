50fddc884ac285963f44a38ffc44880c1426850b
Do Thanh <doxuanthanh23299@gmail.com>
2023-09-20 11:47:33 +0700

    Edit sidebar, head layout

--- resources/views/backend/master/layouts/head.blade.php
+++ resources/views/backend/master/layouts/head.blade.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     <base href="/backend/"/>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/jquery-1.11.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/chart.min.js"></script>
-    <script src="js/chart-data.js"></script>
+    <!-- css -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	<link href="css/styles.css" rel="stylesheet">
+	<!--Icons-->
+	<script src="js/lumino.glyphs.js"></script>
+	<link rel="stylesheet" href="Awesome/css/all.css">
     <script src="../ckeditor/ckeditor.js"></script>
-    <link href="css/styles.css" rel="stylesheet">
 </head>
 
 <body>
--- resources/views/backend/master/layouts/sidebar.blade.php
+++ resources/views/backend/master/layouts/sidebar.blade.php
@@ -2,20 +2,20 @@
     <form role="search">
     </form>
     <ul class="nav menu">
-        <li class="active"><a href="/admin"><svg class="glyph stroked dashboard-dial">
+        <li  class="{{ Request::is('admin') ? 'active' : '' }}"><a href="/admin"><svg class="glyph stroked dashboard-dial">
                     <use xlink:href="#stroked-dashboard-dial"></use>
                 </svg> Tổng quan</a></li>
-        <li><a href="/admin/category"><svg class="glyph stroked clipboard with paper">
+        <li  class="{{ Request::is('admin/category') ? 'active' : '' }}"><a href="/admin/category"><svg class="glyph stroked clipboard with paper">
                     <use xlink:href="#stroked-clipboard-with-paper" />
                 </svg> Danh Mục</a></li>
-        <li><a href="/admin/product"><svg class="glyph stroked bag">
+        <li  class="{{ Request::is('admin/product') ? 'active' : '' }}"><a href="/admin/product"><svg class="glyph stroked bag">
                     <use xlink:href="#stroked-bag"></use>
                 </svg> Sản phẩm</a></li>
-        <li><a href="/admin/order"><svg class="glyph stroked notepad ">
+        <li  class="{{ Request::is('admin/order') ? 'active' : '' }}"><a href="/admin/order"><svg class="glyph stroked notepad ">
                     <use xlink:href="#stroked-notepad" />
                 </svg> Đơn hàng</a></li>
         <li role="presentation" class="divider"></li>
-        <li><a href="/admin/user"><svg class="glyph stroked male-user">
+        <li  class="{{ Request::is('admin/user') ? 'active' : '' }}"><a href="/admin/user"><svg class="glyph stroked male-user">
                     <use xlink:href="#stroked-male-user"></use>
                 </svg> Quản lý thành viên</a></li>
     </ul>
